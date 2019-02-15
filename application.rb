ae0e3664677da6703b5343990741e428b542e33f
Jacob Feldman <jacobgfeld@gmail.com>
2019-02-15 14:52:24 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World\nMy name is Jacob"
+    resp.write "Hello, World\nHello, my name is Jacob"
     resp.finish
   end
 
